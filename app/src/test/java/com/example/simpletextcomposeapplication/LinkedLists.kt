b29430edb260599f698fdcfd0156918f9cd04ff2
Rui Vieira <revs87@msn.com>
2023-01-14 01:51:10 +0000

    Middle calculation and subList arrays added

---
+++ app/src/test/java/com/example/simpletextcomposeapplication/LinkedLists.kt
@@ -0,0 +1,125 @@
+package com.example.simpletextcomposeapplication
+
+import org.junit.Assert
+import org.junit.Test
+
+class LinkedLists {
+
+    /**
+     * Example:
+     * var li = ListNode(5)
+     * var v = li.`val`
+     * Definition for singly-linked list.
+     * class ListNode(var `val`: Int) {
+     *     var next: ListNode? = null
+     * }
+     */
+    private fun isPalindrome(head: ListNode?): Boolean {
+        head ?: return false
+        var digit = head
+
+        val list = ArrayList<Int>()
+        while (digit != null) {
+            list.add(digit.`val`)
+            digit = digit.next
+        }
+
+        var i = 0
+        var j = list.size - 1
+        while (i < list.size / 2) {
+            println("Comparing ${list[i]} with ${list[j]}")
+            if (list[i] != list[j]) {
+                return false
+            }
+            i++
+            j--
+        }
+        return true
+    }
+
+    @Test
+    fun test1() {
+        val nodes = ListNode(1).apply { next = ListNode(2).apply { next = ListNode(2).apply { next = ListNode(1) } } }
+        Assert.assertTrue(isPalindrome(nodes))
+    }
+
+    @Test
+    fun test2() {
+        val nodes = ListNode(1).apply { next = ListNode(2).apply { next = ListNode(3).apply { next = ListNode(2).apply { next = ListNode(1) } } } }
+        Assert.assertTrue(isPalindrome(nodes))
+    }
+
+    @Test
+    fun test3() {
+        val nodes = ListNode(1).apply { next = ListNode(2) }
+        Assert.assertFalse(isPalindrome(nodes))
+    }
+
+    @Test
+    fun test4() {
+        val nodes = ListNode(1)
+        Assert.assertTrue(isPalindrome(nodes))
+    }
+
+    /**
+     * Example:
+     * var li = ListNode(5)
+     * var v = li.`val`
+     * Definition for singly-linked list.
+     * class ListNode(var `val`: Int) {
+     *     var next: ListNode? = null
+     * }
+     *
+     * Input: head = [1,2,3,4,5]
+     * Output: [3,4,5]
+     * Explanation: The middle node of the list is node 3.
+     *
+     * Input: head = [1,2,3,4,5,6]
+     * Output: [4,5,6]
+     * Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+     *
+     */
+    fun middleNodeList(head: ListNode?): IntArray {
+        var index = 0
+        var middleIndex = 0
+        val list = ArrayList<Int>()
+
+        var node = head
+        while (node != null) {
+            list.add(node.`val`)
+            index++
+            if (index % 2 == 0) { middleIndex++ }
+            node = node.next
+        }
+
+        return list.subList(middleIndex, list.size).toIntArray()
+    }
+
+    @Test
+    fun test_middleOf_0() {
+        val nodes: ListNode? = null
+        Assert.assertTrue(intArrayOf().contentEquals(middleNodeList(nodes)))
+    }
+
+    @Test
+    fun test_middleOf_1() {
+        val nodes = ListNode(1)
+        Assert.assertTrue(intArrayOf(1).contentEquals(middleNodeList(nodes)))
+    }
+
+    @Test
+    fun test_middleOf_5() {
+        val nodes = ListNode(1).apply { next = ListNode(2).apply { next = ListNode(3).apply { next = ListNode(4).apply { next = ListNode(5) } } } }
+        Assert.assertTrue(intArrayOf(3, 4, 5).contentEquals(middleNodeList(nodes)))
+    }
+
+    @Test
+    fun test_middleOf_6() {
+        val nodes = ListNode(1).apply { next = ListNode(2).apply { next = ListNode(3).apply { next = ListNode(4).apply { next = ListNode(5).apply { next = ListNode(6) }  } } } }
+        Assert.assertTrue(intArrayOf(4, 5, 6).contentEquals(middleNodeList(nodes)))
+    }
+}
+
+class ListNode(var `val`: Int) {
+    var next: ListNode? = null
+}
