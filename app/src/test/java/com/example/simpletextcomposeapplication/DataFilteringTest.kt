aea8a0457fdf085164538201ac3e6040fb115a8f
Rui Vieira <revs87@msn.com>
2023-01-19 00:24:44 +0000

    isTen regex test code rearrangement

--- app/src/test/java/com/example/simpletextcomposeapplication/DataFilteringTest.kt
+++ app/src/test/java/com/example/simpletextcomposeapplication/DataFilteringTest.kt
@@ -74,16 +74,21 @@ class DataFilteringTest {
         return list.distinctBy { it.lowercase() }
     }
 
-    @Test
-    fun test_isTen() {
-        Assert.assertFalse(isTen("aa6?9"))
-        Assert.assertTrue(isTen("acc?7??sss?3rr1??????5"))
-        Assert.assertTrue(isTen("arrb6??sss?4xxbl5???eee5"))
-    }
-
+    /*
+    *
+    * Have the function QuestionsMarks(str) take the str string parameter, which will contain single
+    *  digit numbers, letters, and question marks, and check if there are exactly 3 question marks
+    *  between every pair of two numbers that add up to 10. If so, then your program should return
+    *  the string true, otherwise it should return the string false. If there aren't any two numbers
+    *  that add up to 10 in the string, then your program should return false as well.
+    * For example: if str is "arrb6???4xxbl5???eee5" then your program should return true because
+    *  there are exactly 3 question marks between 6 and 4, and 3 question marks between 5 and 5 at
+    *  the end of the string.
+    *
+    * */
 
     private fun isTen(str: String): Boolean {
-        val regex = "[1-9][?][\\D]*[?][\\D]*[?][1-9]".toRegex()
+        val regex = "[1-9][?]\\D*[?]\\D*[?][1-9]".toRegex()
 
         if (!regex.containsMatchIn(str)) { return false }
 
@@ -97,4 +102,12 @@ class DataFilteringTest {
 
         return true
     }
+
+    @Test
+    fun test_isTen() {
+        Assert.assertFalse(isTen("aa6?9"))
+        Assert.assertTrue(isTen("acc?7??sss?3rr1??????5"))
+        Assert.assertTrue(isTen("arrb6??sss?4xxbl5???eee5"))
+    }
+
 }
