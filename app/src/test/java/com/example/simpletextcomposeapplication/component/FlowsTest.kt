a999d52e0fb7e634b92d397d6e858cc3345ef1d7
Rui Vieira <revs87@msn.com>
2023-10-03 23:13:50 +0100

    Testing a flow data delayed from 0 to 10 ADDED

--- app/src/test/java/com/example/simpletextcomposeapplication/component/FlowsTest.kt
+++ app/src/test/java/com/example/simpletextcomposeapplication/component/FlowsTest.kt
@@ -1,8 +1,13 @@
 package com.example.simpletextcomposeapplication.component
 
+import app.cash.turbine.test
+import assertk.assertThat
+import assertk.assertions.isEqualTo
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.takeWhile
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
@@ -48,6 +53,25 @@ class FlowsTest {
                 .takeWhile { it.first }
                 .collect { println(it) }
         }
+    }
+
+
+    @Test
+    fun `Testing a flow data delayed from 0 to 10`() = runBlocking {
+        (0..10)
+            .asFlow()
+            .map {
+                delay(100)
+                it
+            }
+            .test {
+                (0..10).forEach {
+                    val emission = awaitItem()
+                    assertThat(emission).isEqualTo(it)
+                }
+//                awaitComplete()
+                cancelAndConsumeRemainingEvents()
+            }
     }
 
 }
