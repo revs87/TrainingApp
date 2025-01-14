61ee5ede8cb911c3bfb51cb43876a8fe647e8913
Rui Vieira <revs87@msn.com>
2025-01-14 23:58:49 +0000

    Advent of Code 2024 - Day 2: Red-Nosed Reports - pt1 FINAL FIX

--- app/src/test/java/com/example/simpletextcomposeapplication/adventofcode/Day02.kt
+++ app/src/test/java/com/example/simpletextcomposeapplication/adventofcode/Day02.kt
@@ -1,13 +1,14 @@
 package com.example.simpletextcomposeapplication.adventofcode
 
+import assertk.assertThat
+import assertk.assertions.isEqualTo
 import com.example.simpletextcomposeapplication.adventofcode.input.readInput
+import kotlinx.coroutines.flow.count
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.runningFold
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Test
-import kotlin.math.abs
 
 
 /**
@@ -15,24 +16,24 @@ import kotlin.math.abs
  * */
 class Day02 {
     @Test
-    fun part1() = runBlocking {
-        flow<List<Int>> {
-            readInput("Day02Example.txt").map { line ->
+    fun part1(): Unit = runBlocking {
+        val expected = 526
+        val sum = flow<List<Int>> {
+//            readInput("Day02Example.txt").map { line ->
+            readInput("Day02Input.txt").map { line ->
                 emit(line.split(" ").map { it.toInt() })
             }
         }.map { report ->
-            report.safeness().also { println(it.id) }
+            report.safeness()//.also { println(it.id + " " + report) }
         }.filter {
             it == Safeness.SAFE
-        }.runningFold(0) { acc, value ->
-            acc + 1
-        }.collect { safeCount ->
-            println(safeCount)
-        }
+        }.count().also(::println)
+
+        assertThat(sum).isEqualTo(expected)
     }
 
     private enum class Safeness(val id: String) {
-        SAFE("Safe"),
+        SAFE("Safe  "),
         UNSAFE("Unsafe")
     }
 
@@ -48,10 +49,16 @@ class Day02 {
             }
             if (isIncreasing == null) {
                 isIncreasing = lastValue < value
-                lastValue = value
-                continue
             }
-            if (isIncreasing != (lastValue < value) || abs(lastValue - value) > 3 || lastValue == value) {
+            val invalidTendency = when (isIncreasing) {
+                true -> lastValue > value
+                false -> lastValue < value
+            }
+            val validAdjacents = when (isIncreasing) {
+                true -> (value - lastValue) in  1..3
+                false -> (lastValue - value) in 1..3
+            }
+            if (lastValue == value || invalidTendency || !validAdjacents) {
                 return Safeness.UNSAFE
             }
             lastValue = value
